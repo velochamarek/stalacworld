3bfbbd40caf4049a66622fe8cde0e1779b8c82cb
anhx <terezaanhhoang@gmail.com>
2026-02-28 02:43:52 +0100

    animace_panacka titulni stranky

---
+++ Assets/Scripts/titulni_walk.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class TitulniPanacek : MonoBehaviour
+{
+    public float rychlost = 2f;
+    public Vector2 cil = new Vector2(16f, -8f);
+    public float delayPoZmizeni = 2f;
+
+    private Vector3 startPosition;
+    private Vector3 startScale;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        startScale = transform.localScale;
+        StartCoroutine(PohybovaSmycka());
+    }
+
+    IEnumerator PohybovaSmycka()
+    {
+        while (true)
+        {
+            Vector3 cilPozice = new Vector3(cil.x, cil.y, startPosition.z);
+
+            while (Vector3.Distance(transform.position, cilPozice) > 0.01f)
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    cilPozice,
+                    rychlost * Time.deltaTime
+                );
+                yield return null;
+            }
+
+            transform.localScale = Vector3.zero;
+            yield return new WaitForSeconds(delayPoZmizeni);
+
+            transform.position = startPosition;
+            transform.localScale = startScale;
+        }
+    }
+}
